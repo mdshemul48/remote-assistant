29aa51845eef3736de801df2b677fde2202fdf52
MD. Shimul <mdshemul480@gmail.com>
2022-01-21 10:28:35 +0600

    removing unused fucntion

--- app.py
+++ app.py
@@ -10,16 +10,6 @@ app = Flask(__name__, static_url_path="/static")
 CORS(app)
 
 
-def convert_sizes(size_bytes):
-    if size_bytes == 0:
-        return "0B"
-    size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
-    i = int(math.floor(math.log(size_bytes, 1024)))
-    p = math.pow(1024, i)
-    s = round(size_bytes / p, 2)
-    return "%s%s" % (s, size_name[i])
-
-
 @app.route("/")
 def home():
     return "you are good to go."
